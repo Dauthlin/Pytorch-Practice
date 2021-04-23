d316078464d80286ecb1d0f5901255027941da6a
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2021-04-23 16:02:51 +0100

    Add files via upload

--- cnn_attempt.py
+++ cnn_attempt.py
@@ -12,22 +12,24 @@ class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         # inherits from the super
-        self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=(3, 3))
+        self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(5, 5))
         # defining our first Convolutional layer
         # in_channels = how many layers does starting image have. We're RGB so its 3
         # out_channels = how many output dimensions do we want
         # kernel_size = the dimensions of the kernel we want to use
         self.pooling = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
         # pooling layer, kernel size is 2x2 and the stride is 2 so the kernel won't look at the same pixels twice
-        self.conv_layer2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=(3, 3))
+        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 5))
         # defining our second Convolutional layer with the input channels the same as the first layers output so they
         # can feed from one into the other.
         self.Relu = nn.ReLU()
         # carries out Relu
-        self.normal_layer1 = nn.BatchNorm2d(12)
+        self.normal_layer1 = nn.BatchNorm2d(16)
         # carries out Zero-centre and normalizes the output of the Convolutional layer before activation
-        self.normal_layer2 = nn.BatchNorm2d(24)
+        self.normal_layer2 = nn.BatchNorm2d(32)
         # carries out Zero-centre and normalizes the output of the Convolutional layer before activation
+        self.linear_layer = nn.Linear(800, len(classes))
+        # output of linear layer is each class
 
     def forward(self, output):
         output = self.conv_layer1(output)
@@ -39,6 +41,12 @@ class ConvolutionalNeuralNetwork(nn.Module):
         output = self.normal_layer2(output)
         output = self.Relu(output)
         output = self.pooling(output)
+        # second Convolutional layer complete
+        output = output.reshape(output.size(0), -1)
+        # we must reshape the output so it can be used by our linear layer
+        output = self.linear_layer(output)
+        # linear layer complete
+        return output
 
 
 if __name__ == '__main__':
@@ -47,20 +55,42 @@ if __name__ == '__main__':
     batch_size = 4
     # the amount of images to use per batch
 
-    training_set = CIFAR10(root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor)
+    training_set = CIFAR10(root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor())
     # train_set contains the CIFAR10 training images, 50000 images, these images have been transformed into tensors
     # it will download the dataset if needed
     training_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True, num_workers=2)
     # train loader contains groups of training images randomly shuffled in groups of 4 (the batch size)
 
-    testing_set = CIFAR10(root='./data', train=False, download=True, transform=torchvision.transforms.ToTensor)
+    testing_set = CIFAR10(root='./data', train=False, download=True, transform=torchvision.transforms.ToTensor())
     # train_set contains the CIFAR10 testing images, 10000 images, these images have been transformed into tensors
     testing_loader = DataLoader(testing_set, batch_size=batch_size, shuffle=False, num_workers=2)
     # test loader contains groups of testing images randomly shuffled in groups of 4 (the batch size)
 
     classes = training_set.classes
-    #print(len(training_set))
+    # print(len(training_set))
 
-    loss_function = nn.CrossEntropyLoss
+    loss_function = nn.CrossEntropyLoss()
+    learning_rate = 0.001
+    ConvolutionalNeuralNetwork = ConvolutionalNeuralNetwork()
+    optimizer = torch.optim.SGD(ConvolutionalNeuralNetwork.parameters(), lr=learning_rate, momentum=0.9)
 
+    # optimizer using stochastic gradient decent
 
+    for loops in range(2):  # how many loops over the data you want to do
+        for training_data in training_loader:
+            inputs, labels = training_data
+            # gets input values and labels
+
+            optimizer.zero_grad()
+            # zeros the gradients
+
+            outputs = ConvolutionalNeuralNetwork(inputs)
+            # generates outputs
+
+            loss = loss_function(outputs, labels)
+            # applies loss function to outputs
+            loss.backward()
+            # generates the gradients (back propagation)
+            optimizer.step()
+            # applies stochastic gradient decent, so only a small sample is used to save time
+        print(loss.item())
